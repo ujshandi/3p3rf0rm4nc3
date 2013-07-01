af19d9ff86dab90d32f5900daafe91a4b5226c90
ujshandi@gmail.com <ujshandi@gmail.com@96486279-669c-5b07-b16b-ce5d5bd70c50>
2013-07-01 04:29:58 +0000

    -rarapih

--- application/views/portal/bottom_vw.php
+++ application/views/portal/bottom_vw.php
@@ -8,7 +8,7 @@
             <div class="copyrights">&copy; e-Perfomance 2013 - <a href="#" target="_blank">Sistem Informasi Pengukuran Kinerja Kementerian Perhubungan RI</a></div>
             <div class="footer_right_links">
                 <ul>
-                    <li class="selected"><a href="index.html">HOME</a></li>
+                    <li class="selected"><a href="<?=base_url();?>">HOME</a></li>
                     <li><a href="#">KONTAK</a></li>
                     <li><a href="#">WEB MAIL</a></li>
                     <li><a href="#">LOGIN</a></li>
