dc0ff3add5107bd498338c579ca38a4b1e9cb7b1
stefanus.christian.c@gmail.com <stefanus.christian.c@gmail.com@96486279-669c-5b07-b16b-ce5d5bd70c50>
2013-07-01 04:05:55 +0000

    Pembenahan bug jika data kosong.

--- trunk/application/views/portal/faq_vw.php
+++ trunk/application/views/portal/faq_vw.php
@@ -3,12 +3,12 @@
         <div class="page_title"><h1>FAQ</h1></div>
         <hr/>
         <ul id="faq">
-                <? foreach ($faqs->result() as $faq) :?>
+                <? if($faqs){foreach ($faqs->result() as $faq) :?>
                 <li>
                     <h3><a href="#"><?=$faq->content?></a></h3>
                     <img src="<?=base_url();?>public/images/portal/ans-icon.png" /><p>"<?=$faq->summary?>"</p>
                 </li>
-                <? endforeach;?>
+                <? endforeach;}else echo '<h1>Saat ini data FAQ masih kosong</h1>';?>
             </ul>
         <div class="clear"></div>
     </div><!-- end center content -->
