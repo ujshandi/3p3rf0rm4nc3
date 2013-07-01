82d09b48fdcfc963d5fb327ae47f0aa9ddbc9165
ujshandi@gmail.com <ujshandi@gmail.com@96486279-669c-5b07-b16b-ce5d5bd70c50>
2013-07-01 10:12:07 +0000

    - checkpoint e1

--- application/models/checkpoint/checkpointe1_model.php
+++ application/models/checkpoint/checkpointe1_model.php
@@ -2,7 +2,7 @@
 /**
  *INVISI
 */
-
+  
 class Checkpointe1_model extends CI_Model
 {	
 	/**
@@ -14,13 +14,12 @@ class Checkpointe1_model extends CI_Model
 		//$this->CI =& get_instance();
     }
 	
-	public function easyGrid($filtahun=null,$file1=null){
+	public function easyGrid($filtahun=null){
 		
 		$page = isset($_POST['page']) ? intval($_POST['page']) : 1;  
 		$limit = isset($_POST['rows']) ? intval($_POST['rows']) : 10;  
 		
-		$count = $this->GetRecordCount($filtahun,$file1);
-	//	var_dump($count);die;	
+		$count = $this->GetRecordCount($filtahun);
 		$response = new stdClass();
 		$response->total = $count;
 		$sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'tahun';  
@@ -32,18 +31,15 @@ class Checkpointe1_model extends CI_Model
 			//filter
 			if($filtahun != '' && $filtahun != '-1' && $filtahun != null) {
 				$this->db->where("tbl_pk_eselon1.tahun",$filtahun);
-			}
-			if($file1 != '' && $file1 != '-1' && $file1 != null) {
-				$this->db->where("tbl_pk_eselon1.kode_e1",$file1);
-			}
+			}	
+			
 			$this->db->order_by($sort." ".$order );
 			$this->db->limit($limit,$offset);
-			//$this->db->select("*, tbl_pk_eselon1.kode_e1 as pk_kode_e1",false);
-			$this->db->select("tbl_pk_eselon1.*, tbl_iku_eselon1.deskripsi as deskripsi_iku_e1,tbl_iku_eselon1.satuan, tbl_sasaran_eselon1.deskripsi as deskripsi_sasaran_e1, tbl_eselon1.nama_e1",false);
-			$this->db->from('tbl_pk_eselon1');
-			$this->db->join('tbl_iku_eselon1', 'tbl_iku_eselon1.kode_iku_e1 = tbl_pk_eselon1.kode_iku_e1 and tbl_iku_eselon1.tahun = tbl_pk_eselon1.tahun','left');
-			$this->db->join('tbl_sasaran_eselon1', 'tbl_sasaran_eselon1.kode_sasaran_e1 = tbl_pk_eselon1.kode_sasaran_e1 and tbl_sasaran_eselon1.tahun = tbl_pk_eselon1.tahun','left');
-			$this->db->join('tbl_eselon1', 'tbl_eselon1.kode_e1 = tbl_pk_eselon1.kode_e1 ');
+			$this->db->select("distinct tbl_pk_eselon1.*,tbl_iku_eselon1.deskripsi as deskripsi_iku_e1,tbl_iku_eselon1.satuan,tbl_sasaran_eselon1.deskripsi as deskripsi_sasaran_e1, tbl_eselon1.nama_e1",false);
+			$this->db->from('tbl_pk_eselon1 ');
+			$this->db->join('tbl_iku_eselon1','tbl_iku_eselon1.kode_iku_e1 = tbl_pk_eselon1.kode_iku_e1 and tbl_iku_eselon1.tahun = tbl_pk_eselon1.tahun');
+			$this->db->join('tbl_sasaran_eselon1','tbl_sasaran_eselon1.kode_sasaran_e1 = tbl_pk_eselon1.kode_sasaran_e1');
+			$this->db->join('tbl_eselon1', 'tbl_eselon1.kode_e1 = tbl_pk_eselon1.kode_e1');
 			$this->db->order_by("tbl_pk_eselon1.tahun DESC, kode_sasaran_e1 ASC, tbl_pk_eselon1.kode_iku_e1 ASC");
 			$query = $this->db->get();
 			
@@ -85,57 +81,99 @@ class Checkpointe1_model extends CI_Model
 				$response->rows[$i]['satuan']=$row->satuan;
 				$i++;
 			} 
-			
 			$query->free_result();
 		}else {
 				$response->rows[$count]['id_pk_e1']='';
 				$response->rows[$count]['tahun']='';
 				$response->rows[$count]['kode_e1']='';
-				$response->rows[$count]['nama_e1']='';
 				$response->rows[$count]['kode_sasaran_e1']='';
 				$response->rows[$count]['kode_iku_e1']='';
 				$response->rows[$count]['target']='';
-				$response->rows[$count]['penetapan']='';
 				$response->rows[$count]['satuan']='';
+				$response->rows[$count]['penetapan']='';
+				$response->rows[$count]['nama_e1']='';
 				$response->rows[$count]['deskripsi_iku_e1']='';
 				$response->rows[$count]['deskripsi_sasaran_e1']='';
+		}
+		
+		return json_encode($response);
+	}
+	
+	public function easyGridDetail($id_pk){
+		
+		$page = isset($_POST['page']) ? intval($_POST['page']) : 1;  
+		$limit = isset($_POST['rows']) ? intval($_POST['rows']) : 10;  
+		
+		//$count = $this->GetRecordCount($filtahun);
+		$response = new stdClass();
+	//	$response->total = $count;
+		$sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'periode';  
+		$sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'tbl_pk_eselon1.tahun';  
+		$order = isset($_POST['order']) ? strval($_POST['order']) : 'desc';  
+		$offset = ($page-1)*$limit;  
+		$count = 0;
+		//if ($count>0){
+			//filter
+			//if($filtahun != '' && $filtahun != '-1' && $filtahun != null) {
+				$this->db->where("tbl_checkpoint_e1.id_pk_e1",$id_pk);
+			//}	
+			
+			//$this->db->order_by($sort." ".$order );
+		//	$this->db->limit($limit,$offset);
+			$this->db->select("distinct tbl_checkpoint_e1.*",false);
+			$this->db->from('tbl_checkpoint_e1 ');
+			
+			$this->db->order_by("tbl_checkpoint_e1.periode DESC");
+			$query = $this->db->get();
+			
+			$i=0;
+			foreach ($query->result() as $row)
+			{
+				$response->rows[$i]['id_pk_e1']=$row->id_pk_e1;
+				$response->rows[$i]['id_checkpoint_e1']=$row->id_checkpoint_e1;
+				$response->rows[$i]['unit_kerja']=$row->unit_kerja;
+				$response->rows[$i]['keterangan']=$row->keterangan;
+				$response->rows[$i]['periode']=$row->periode;
+				$response->rows[$i]['kriteria']=$row->kriteria;
+				$response->rows[$i]['ukuran']=$row->ukuran;
+				$response->rows[$i]['target']=$this->utility->cekNumericFmt($row->target);
+				$response->rows[$i]['capaian']=$this->utility->cekNumericFmt($row->capaian);
+				
+
+
+				$i++;
+			} 
+			$query->free_result();
+		 if ($i==0) {
+				$response->rows[$count]['id_pk_e1']='';
+				$response->rows[$count]['id_checkpoint_e1']='';
+				$response->rows[$count]['periode']='';
+				$response->rows[$count]['kriteria']='';
+				$response->rows[$count]['ukuran']='';
+				$response->rows[$count]['target']='';
+				$response->rows[$count]['capaian']='';
 				
 		}
 		
 		return json_encode($response);
-		
 	}
 	
-	public function GetRecordCount($filtahun=null,$file1){
+	
+	public function GetRecordCount($filtahun=null){
 		if($filtahun != '' && $filtahun != '-1' && $filtahun != null) {
 			$this->db->where("tbl_pk_eselon1.tahun",$filtahun);
-		}	
-		if($file1 != '' && $file1 != '-1' && $file1 != null) {
-			$this->db->where("tbl_pk_eselon1.kode_e1",$file1);
-		}		
-		//$this->db->select('distinct tbl_pk_eselon1.*, tbl_iku_eselon1.kode_e1 as pk_kode_e1');
-		$this->db->from('tbl_pk_eselon1');
-		$this->db->join('tbl_iku_eselon1', 'tbl_iku_eselon1.kode_iku_e1 = tbl_pk_eselon1.kode_iku_e1 and tbl_iku_eselon1.tahun = tbl_pk_eselon1.tahun','left');
-		$this->db->join('tbl_sasaran_eselon1', 'tbl_sasaran_eselon1.kode_sasaran_e1 = tbl_pk_eselon1.kode_sasaran_e1 and tbl_sasaran_eselon1.tahun = tbl_pk_eselon1.tahun','left');
-			
-		//$this->db->order_by("tbl_pk_eselon1.tahun DESC, kode_sasaran_e1 ASC, tbl_pk_eselon1.kode_iku_e1 ASC");
-	
+		}
+		
+		$this->db->select("distinct tbl_pk_eselon1.*,tbl_iku_eselon1.deskripsi as deskripsi_iku_e1,tbl_iku_eselon1.satuan,tbl_sasaran_eselon1.deskripsi as deskripsi_sasaran_e1, tbl_eselon1.nama_e1",false);
+		$this->db->from('tbl_pk_eselon1 ');
+		$this->db->join('tbl_iku_eselon1','tbl_iku_eselon1.kode_iku_e1 = tbl_pk_eselon1.kode_iku_e1 and tbl_iku_eselon1.tahun = tbl_pk_eselon1.tahun');
+		$this->db->join('tbl_sasaran_eselon1','tbl_sasaran_eselon1.kode_sasaran_e1 = tbl_pk_eselon1.kode_sasaran_e1');
+		$this->db->join('tbl_eselon1', 'tbl_eselon1.kode_e1 = tbl_pk_eselon1.kode_e1');
+		
 		return $this->db->count_all_results();
 		$this->db->free_result();
 	}
 	
-	public function getDataEdit($id){
-		$this->db->flush_cache();
-		$this->db->select('*, b.deskripsi as sasaran, c.deskripsi as iku_e1, d.nama_e1 as namae1');
-		$this->db->from('tbl_pk_eselon1 a');
-		$this->db->join('tbl_sasaran_eselon1 b', 'b.kode_sasaran_e1 = a.kode_sasaran_e1');
-		$this->db->join('tbl_iku_eselon1 c', 'c.kode_iku_e1 = a.kode_iku_e1 and c.tahun = a.tahun');
-		$this->db->join('tbl_eselon1 d', 'd.kode_e1 = a.kode_e1');
-		$this->db->where('a.id_pk_e1', $id);
-		
-		return $this->db->get()->row();
-	}
-
 	public function getDetail($tahun, $kode_e1, $kode_sasaran_e1){
 		$out = '';
 		$i = 1;
@@ -197,7 +235,7 @@ class Checkpointe1_model extends CI_Model
 		# -------------------------------------------------------------------------------------
 		$this->db->flush_cache();
 		$this->db->select('*');
-		$this->db->from('tbl_rkt_eselon1 a');
+		$this->db->from('tbl_rkt_kl a');
 		$this->db->join('tbl_iku_eselon1 b','a.kode_iku_e1 = b.kode_iku_e1 and a.tahun = b.tahun');
 		$this->db->where('a.tahun', $tahun);
 		$this->db->where('a.kode_e1', $kode_e1);
@@ -235,38 +273,53 @@ class Checkpointe1_model extends CI_Model
 		
 		//chan 
 		if ($out=="")
-			return $out .= 'Data RKT Eselon 1 belum tersedia.';
-		
+			return $out .= 'Data RKT Kementerian belum tersedia.';
+				
 		return $header.$out;
 	}
+	
+	public function getDataEdit($id){
+		$this->db->flush_cache();
+		$this->db->select('*, b.deskripsi as sasaran, c.deskripsi as iku_e1');
+		$this->db->from('tbl_pk_eselon1 a');
+		$this->db->join('tbl_sasaran_eselon1 b', 'b.kode_sasaran_e1 = a.kode_sasaran_e1');
+		$this->db->join('tbl_iku_eselon1 c', 'c.kode_iku_e1 = a.kode_iku_e1 and c.tahun = a.tahun');
+		$this->db->join('tbl_eselon1 d', 'd.kode_e1 = a.kode_e1');
+		$this->db->where('a.id_pk_e1', $id);
 		
+		return $this->db->get()->row();
+	}
+	
 	public function InsertOnDB($data) {
 		$this->db->trans_start();
 		
-		foreach($data['detail'] as $dt){
+		//foreach($data['detail'] as $dt){
 			
 			# jika id_pk_e1 != 0 maka update
+/*
 			if($dt['id_pk_e1'] != '0'){
 				// update target penetapan di tabel PK
 				$this->db->flush_cache();
-				$this->db->set('penetapan', $dt['penetapan']);
+				$this->db->set('penetapan', 	$dt['penetapan']);
 				$this->db->set('log_update', 	$this->session->userdata('user_id').';'.date('Y-m-d H:i:s'));
-				$this->db->where('id_pk_e1', $dt['id_pk_e1']);
+				$this->db->where('id_pk_e1', 	$dt['id_pk_e1']);
 				$this->db->update('tbl_pk_eselon1');
 				
 			}else{
-				//query insert data ke PK
-				$this->db->flush_cache();
-				$this->db->set('tahun',$data['tahun']);
-				$this->db->set('kode_e1',$data['kode_e1']);
-				$this->db->set('kode_sasaran_e1',$data['kode_sasaran_e1']);
-				$this->db->set('kode_iku_e1',$dt['kode_iku_e1']);
-				$this->db->set('target',$dt['target']);
-				$this->db->set('penetapan',$dt['penetapan']);
-				$this->db->set('status','0');
+*/
+				//query insert data ke checkpoint				$this->db->flush_cache();
+				$this->db->set('id_pk_e1',$data['id_pk_e1']);
+				$this->db->set('unit_kerja',$data['unit_kerja']);
+				$this->db->set('periode',$data['periode']);
+				$this->db->set('kriteria',$data['kriteria']);
+				$this->db->set('ukuran',$data['ukuran']);
+				$this->db->set('target',$data['target']);
+				$this->db->set('keterangan',$data['keterangan']);
+				$this->db->set('capaian',$data['capaian']);
 				$this->db->set('log_insert', 	$this->session->userdata('user_id').';'.date('Y-m-d H:i:s'));
-				$result = $this->db->insert('tbl_pk_eselon1');
+				$result = $this->db->insert('tbl_checkpoint_e1');
 				
+/*
 				// update status tabel RKT
 				$this->db->flush_cache();
 				$this->db->set('status', '1');
@@ -274,7 +327,7 @@ class Checkpointe1_model extends CI_Model
 				$this->db->where('kode_e1', $data['kode_e1']);
 				$this->db->where('kode_sasaran_e1', $data['kode_sasaran_e1']);
 				$this->db->where('kode_iku_e1', $dt['kode_iku_e1']);
-				$this->db->update('tbl_rkt_eselon1');
+				$this->db->update('tbl_rkt_kl');
 				
 				# insert to log
 				$this->db->flush_cache();
@@ -287,13 +340,14 @@ class Checkpointe1_model extends CI_Model
 				$this->db->set('status','0');
 				$this->db->set('log',				'INSERT;'.$this->session->userdata('user_id').';'.date('Y-m-d H:i:s'));
 				$result = $this->db->insert('tbl_pk_eselon1_log');
+*/
 				
-			}
+			//}
 			
-		}
-		
-		
+		//}
+	
 		$this->db->trans_complete();
+			//print_r($this->db);die;
 	    return $this->db->trans_status();
 	}
 	
@@ -301,7 +355,7 @@ class Checkpointe1_model extends CI_Model
 		$this->db->flush_cache();
 		$this->db->where('id_pk_e1', $data['id_pk_e1']);
 		$this->db->set('penetapan', $data['penetapan']);
-		$result = $this->db->update('tbl_pk_eselon1', $data); 
+		$result = $this->db->update('tbl_pk_eselon1', $data);
 		
 		# insert to log
 		$this->db->flush_cache();
@@ -321,7 +375,6 @@ class Checkpointe1_model extends CI_Model
 		$result = $this->db->insert('tbl_pk_eselon1_log');
 		
 		
-		
 		$errNo   = $this->db->_error_number();
 	    $errMess = $this->db->_error_message();
 		$error = $errMess;
@@ -337,7 +390,7 @@ class Checkpointe1_model extends CI_Model
 	
 	//hapus data
 	public function DeleteOnDb($id){
-		
+	
 		# insert to log
 		$this->db->flush_cache();
 		$this->db->select("*");
@@ -355,9 +408,11 @@ class Checkpointe1_model extends CI_Model
 		$this->db->set('log',				'DELETE;'.$this->session->userdata('user_id').';'.date('Y-m-d H:i:s'));
 		$result = $this->db->insert('tbl_pk_eselon1_log');
 		
+		// proses
 		$this->db->flush_cache();
 		$this->db->where('id_pk_e1', $id);
-		$result = $this->db->delete('tbl_pk_eselon1'); 
+		$result = $this->db->delete('tbl_pk_eselon1');
+		
 		
 		$errNo   = $this->db->_error_number();
 	    $errMess = $this->db->_error_message();
@@ -373,27 +428,12 @@ class Checkpointe1_model extends CI_Model
 		}
 	}
 	
-	public function getSatuan($id){
-		$this->db->flush_cache();
-		$this->db->select('satuan');
-		$this->db->from('tbl_iku_eselon1');
-		$this->db->where('kode_iku_e1', $id);
-		$query = $this->db->get();
-		
-		return $query->row()->satuan;
-		
-	}
-	
 	public function getListFilterTahun($objectId){
 		
 		$this->db->flush_cache();
 		$this->db->select('distinct tahun',false);
 		$this->db->from('tbl_pk_eselon1');
-		$e1 = $this->session->userdata('unit_kerja_e1');
-		if (($e1!="-1")&&($e1!=null)){
-			$this->db->where('kode_e1',$e1);
-			//$value = $e1;
-		}
+		
 		$this->db->order_by('tahun');
 		
 		$que = $this->db->get();
@@ -409,17 +449,11 @@ class Checkpointe1_model extends CI_Model
 		echo $out;
 	}
 	
-	
-	public function getListTahun($objectId=''){
+	public function getListTahun($objectId=""){
 		
 		$this->db->flush_cache();
 		$this->db->select('tahun');
 		$this->db->from('tbl_pk_eselon1');
-		$e1 = $this->session->userdata('unit_kerja_e1');
-		if (($e1!="-1")&&($e1!=null)){
-			$this->db->where('kode_e1',$e1);
-			//$value = $e1;
-		}
 		$this->db->group_by('tahun');
 		
 		$que = $this->db->get();
@@ -429,13 +463,13 @@ class Checkpointe1_model extends CI_Model
 		foreach($que->result() as $r){
 			$out .= '<option value="'.$r->tahun.'">'.$r->tahun.'</option>';
 		}
+		$out .= '</select>';
 		
 		//chan
 		if ($que->num_rows()==0){
 			$out = "Data Program PK belum tersedia.";
 		}
 		
-		$out .= '</select>';
 		
 		echo $out;
 	}
