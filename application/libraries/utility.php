2fd64c390b2cee9c7633af7c753a9122e1586fa4
ujshandi@gmail.com <ujshandi@gmail.com@96486279-669c-5b07-b16b-ce5d5bd70c50>
2013-08-14 10:38:34 +0000

    backup

--- application/libraries/utility.php
+++ application/libraries/utility.php
@@ -333,6 +333,33 @@ function getListBulan($value,$name=null){
 }
 
 
+function chmod_R($path, $filemode) { 
+    if (!is_dir($path))
+       return chmod($path, $filemode);
+
+    $dh = opendir($path);
+    while ($file = readdir($dh)) {
+        if($file != '.' && $file != '..') {
+            $fullpath = $path.'/'.$file;
+            if(!is_dir($fullpath)) {
+              if (!chmod($fullpath, $filemode))
+                 return FALSE;
+            } else {
+              if (!chmod_R($fullpath, $filemode))
+                 return FALSE;
+            }
+        }
+    }
+ 
+    closedir($dh);
+    
+    if(chmod($path, $filemode))
+      return TRUE;
+    else 
+      return FALSE;
+} 
+
+
 function getListCheckpoint($value,$name=null){
 	if ($name == null)
 		$tmp = "<select name='cmbPeriode' id='cmbPeriode'>";
